b04e1a3bb1dd3d311d06977a35ae2459a518f0e9
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-07 18:00:25 +0000

    install axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  // api call
+  const [countriesData, setCountriesData] = useState(null)
+
+  useEffect(() => {
+    const baseURL = `https://restcountries.com/v3.1/all`
+      axios.get(baseURL).then((response) => {
+        setCountriesData(response.data)
+      })
+  }, [])
+
+  console.log(countriesData)
+
   return (
     <div className="App">
       <p>it works...</p>
