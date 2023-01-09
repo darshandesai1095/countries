f4597e05f026688fa803d3c2fce122e938a8cc4d
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-09 17:44:29 +0000

    country card text formatting

--- src/components/countryCard/CountryCard.js
+++ src/components/countryCard/CountryCard.js
@@ -3,15 +3,19 @@ import './CountryCard.css'
 
 export default function CountryCard(props) {
 
+    const population = props.country.population.toLocaleString("en-US")
+    const region = props.country.region
+    const capital = props.country.capital
+
     return (
         <div className="country-card">
 
             <img src={props.country.flags.png}/>
             <div className="description">
                 <h4 className="title">{props.country.name.common}</h4>
-                <p><span className="bold">Population: </span>{props.country.population}</p>
-                <p><span className="bold">Region: </span>{props.country.region}</p>
-                <p><span className="bold">Capital: </span>{props.country.capital}</p>
+                <p><span className="bold">Population: </span>{population}</p>
+                <p><span className="bold">Region: </span>{region}</p>
+                <p><span className="bold">Capital: </span>{capital}</p>
             </div>
 
         </div>
