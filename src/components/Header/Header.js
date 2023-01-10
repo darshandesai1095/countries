9a1d64d6c3ddec88413b3dcd4ca204f6576cb33b
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-10 21:40:44 +0000

    add dark mode icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
-
+import './Header.css'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 
 function Header() {
 
 
     return (
-        <p>it works</p>
+        <div className='header'>
+            <h1>Where in the world?</h1>
+            <div className='header__darkmode'>
+                <DarkModeOutlinedIcon className='darkmode-icon'/>
+                <p>Dark Mode</p>
+            </div>
+        </div>
     )
 }
 
