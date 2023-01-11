a7bf2bd6db6d41fb766a62f663f9358bcd08520e
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-11 22:03:56 +0000

    style search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import './SearchBar.css'
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { setSearch } from '../../redux/features/searchSlice'
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
 
@@ -10,11 +11,16 @@ function SearchBar() {
 
     return (
         <div className='search-bar'>
+
+            <SearchIcon className='search-icon'/>
+
             <input
                 type="text"
+                placeholder="Search for a country..."
                 value={search.searchEntry}
                 onChange={(e) => dispatch(setSearch(e.target.value))}
             />
+
         </div>
     )
 }
