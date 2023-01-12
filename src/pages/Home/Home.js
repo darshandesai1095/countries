5a96365c21d5752a5ea49d7eb4a9a953397891fb
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-12 23:21:13 +0000

    dark theme backgeound

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import './Home.css'
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 import CountryCard from '../../components/CountryCard/CountryCard';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Dropdown from '../../components/Dropdown/Dropdown';
@@ -19,9 +20,22 @@ function Home() {
         console.log(countriesData)
     }, [])
 
+    const theme = useSelector((state) => state.theme.value)
+
+    const home = {
+        darkMode: {
+            color: 'white',
+            backgroundColor: '#324554',
+        },
+        lightMode: {
+            color: '#212529',
+            backgroundColor: '#f8f9fa',
+        }
+    }
+
     return (
 
-        <div className='home'>
+        <div className='home' style={theme.darkTheme? home.lightMode : home.darkMode}>
 
             <div className='home__nav'>
                 <SearchBar/>
