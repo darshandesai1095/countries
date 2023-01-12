dba6986ccbd9bafb8a44b323dd808cc789152aa3
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-12 22:43:49 +0000

    header dark mode theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,16 +7,34 @@ import { switchTheme } from '../../redux/features/themeSlice'
 
 function Header() {
 
+    const header = {
+        darkMode: {
+            backgroundColor: '#013a63',
+            boxShadow: '0px 3px 5px #e9ecef',
+            color: 'white'
+        },
+        lightMode: {
+            backgroundColor: 'white',
+            boxShadow: '0px 3px 5px #e9ecef'
+        }
+    }
+
+
+
     const theme = useSelector((state) => state.theme.value)
     const dispatch = useDispatch()
 
     return (
-        <div className='header'>
+        <div className='header' style={ theme.darkTheme? header.lightMode : header.darkMode }>
             <h1>Where in the world?</h1>
             <div className='header__darkmode' onClick={() => dispatch(switchTheme())}>
-                <p>{theme.darkTheme? "dark" : "light"}</p>
-                <DarkModeOutlinedIcon className='darkmode-icon'/>
-                <p>Dark Mode</p>
+                {
+                 theme.darkTheme ?
+                 <DarkModeOutlinedIcon className='darkmode-icon'/>
+                 :
+                 <DarkModeIcon className='darkmode-icon'/>
+                }
+                <p>{theme.darkTheme? "Light Mode" : "Dark Mode"}</p>
             </div>
         </div>
     )
