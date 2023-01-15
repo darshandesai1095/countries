2124437fe4000dcc478efbca5662cf7e2caed0b0
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-15 15:38:31 +0000

    search icon dark theme

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,12 +7,26 @@ import SearchIcon from '@mui/icons-material/Search';
 function SearchBar() {
 
     const search = useSelector((state) => state.search.value)
+    const theme = useSelector((state) => state.theme.value)
     const dispatch = useDispatch()
 
-    return (
-        <form className='search-bar'>
+    const searchBar = {
+        darkMode: {
+            color: 'white',
+            backgroundColor: '#435567',
+            border: 'none'
+        },
+        lightMode: {
+            color: '#212529',
+            backgroundColor: 'white',
+        }
+    }
 
-            <SearchIcon className='search-icon'/>
+    return (
+        <form className={`search-bar ${theme.darkTheme? "search-bar--light" : "search-bar--dark"}`} >
+
+            
+            <SearchIcon className={`search-iron ${theme.darkTheme? "search-icon--light" : "search-icon--dark"}`} />
 
             <input
                 type="text"
