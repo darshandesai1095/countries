80d64ebb662e8a168f9b84111320212efeabef39
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-17 22:20:16 +0000

    searchbar dark theme

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,24 +11,53 @@ function SearchBar() {
     const dispatch = useDispatch()
 
     const searchBar = {
-        darkMode: {
-            color: 'white',
-            backgroundColor: '#435567',
-            border: 'none'
-        },
-        lightMode: {
-            color: '#212529',
+        light: {
             backgroundColor: 'white',
+            boxShadow: '0px 0px 10px #dee2e6',
+            border: '1px solid white'
+        },
+        dark: {
+            backgroundColor: '#435567',
+            boxShadow: '0px 0px 10px #304346',
+            border: '1px solid #304346'
+
+        }
+    }
+
+    const searchIcon = {
+        light: {
+            color: '#6c757d',
+            border: '1px solid white',
+            transition: '0.2s'
+        },
+        dark: {
+            color: 'white',
+            border: '1px solid #435567',
+            transition: '0.2s'
+        }
+    }
+
+    const inputElStyle = {
+        light: {
+            color: '#343a40'
+        },
+
+        dark: {
+            color: 'white',
+            backgroundColor: '#435567'
         }
     }
 
     return (
-        <form className={`search-bar ${theme.darkTheme? "search-bar--light" : "search-bar--dark"}`} >
+        <form   className={`search-bar ${theme.darkTheme ? "search-bar--light" : "search-bar--dark"}`}
+                style={theme.darkTheme ? searchBar.light : searchBar.dark} >
 
             
-            <SearchIcon className={`search-icon ${theme.darkTheme? "search-icon--light" : "search-icon--dark"}`} />
+            <SearchIcon className={`search-icon ${theme.darkTheme ? "search-icon--light" : "search-icon--dark"}`}
+                        style={theme.darkTheme ? searchIcon.light : searchIcon.dark}/>
 
             <input
+                style={theme.darkTheme ? inputElStyle.light : inputElStyle.dark}
                 type="text"
                 placeholder="Search for a country..."
                 value={search.searchEntry}
