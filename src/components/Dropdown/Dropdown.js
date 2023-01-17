7f9166291c8b43b310c9f2f8e5ceda938e6eeae0
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-17 20:51:55 +0000

    dropdown menu dark theme

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -8,7 +8,37 @@ function Dropdown() {
     const [dropdownActive, setDropdownActive] = useState(false)
     const regions = ["Africa", "America", "Asia", "Europe", "Oceana"]
     const search = useSelector((state) => state.search.value)
+    const theme = useSelector((state) => state.theme.value)
     const dispatch = useDispatch()
+
+    const buttonStyle = {
+        light: {
+            backgroundColor: 'white',
+            boxShadow:'0px 0px 10px #dee2e6',
+            color: '#495057',
+            border: '1px solid white'
+        },
+
+        dark: {
+            backgroundColor: '#435567',
+            boxShadow:'0px 0px 10px #304346',
+            color: 'white',
+            border: '1px solid #435567'
+        }
+    }
+
+    const dropdownMenuStyle = {
+        light: {
+            backgroundColor: 'white',
+            color: '#495057',
+        },
+
+        dark: {
+            backgroundColor: '#435567',
+            color: 'white',
+
+        }
+    }
 
     return (
         <div 
@@ -16,7 +46,7 @@ function Dropdown() {
             onMouseLeave={() => setDropdownActive(false)}
         >
 
-            <button>
+            <button style={theme.darkTheme ? buttonStyle.light : buttonStyle.dark}>
                 <span>Filter by Region</span>
                 <span className='dropdown__bold'>⌄</span>
             </button>
@@ -26,8 +56,8 @@ function Dropdown() {
            {
                 dropdownActive
                 &&
-                <div className='dropdown__options'>
-                    {regions.map(region => (<p>{region}</p>))}
+                <div className='dropdown__options' style={theme.darkTheme ? dropdownMenuStyle.light : dropdownMenuStyle.dark}>
+                    {regions.map(region => (<p style={theme.darkTheme ? dropdownMenuStyle.light : dropdownMenuStyle.dark}>{region}</p>))}
                 </div>
             }
 
