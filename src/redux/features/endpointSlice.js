31290f20a7c0655d360ab1f1e2eb7d37ff18289e
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-18 00:30:39 +0000

    create endpoint slice

---
+++ src/redux/features/endpointSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { 
+    value: {
+        endpoint: 'all'
+    },
+}
+
+export const endpointSlice = createSlice({
+  name: 'endpoint',
+  initialState,
+  reducers: {
+    setEndpoint: (state) => {
+      state.value.endpoint = action.payload
+    },
+  },
+})
+
+
+export const { setEndpoint } = endpointSlice.actions
+export default endpointSlice.reducer
