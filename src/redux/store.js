4ca16c5c9821672e67089d063694bad5dfb9be79
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-18 00:30:28 +0000

    add endpoint reducer to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from './features/themeSlice'
 import searchReducer from './features/searchSlice'
+import endpointReducer from './features/endpointSlice'
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
-    search: searchReducer
+    search: searchReducer,
+    endpoint: endpointReducer
+
   },
 })
