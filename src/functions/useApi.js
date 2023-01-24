eb76294e8c96fac3db63d0a24454831fd24c5e70
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-24 23:56:08 +0000

    update useApi custom hook

---
+++ src/functions/useApi.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux'
+import axios from 'axios';
+
+function useApi() {
+
+    // api call
+    const [countriesData, setCountriesData] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const endpoint = useSelector((state) => state.endpoint.value.endpoint)
+
+    useEffect(() => {
+        const baseURL = `https://restcountries.com/v3.1/${endpoint}`
+        axios.get(baseURL).then((response) => {
+            setLoading(true)
+            setCountriesData(response.data)
+        }).catch(error => {
+            // "Request failed with status code 404")
+            setError(error.message)
+        }).finally(setLoading(false))
+    }, [endpoint])
+
+    return (
+        {countriesData, error, loading}
+    )
+}
+
+export default useApi
