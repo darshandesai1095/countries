00916e25586a298b0bd2189d4dbfebbde19dd641
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-26 19:33:48 +0000

    add country info

--- src/components/CountryInfo/CountryInfo.js
+++ src/components/CountryInfo/CountryInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useApi from '../../functions/useApi';
+import './CountryInfo.css'
 
 function CountryInfo() {
 
@@ -7,28 +8,32 @@ const country = useApi().countriesData[0]
 console.log("COUNTRY: ", country)
 
   return (
-    <div>
+    <div className='country-info'>
+
         <img src={country.flags?.png}/>
-        <h4>{country.name.common}</h4>
 
-        <div className='country__primary-info'>
-            <p><span>Native Name:</span> {country.name.official}</p>
-            <p><span>Population:</span> {country.name.official}</p>
-            <p>Region: {country.region}</p>
-            <p>Sub Region: {country.subregion}</p>
-            <p>Capital: {country.capital}</p>
+        <div className=''>
+
+            <h4>{country.name.common}</h4>
+
+            <div className='country__primary-info'>
+                <p><span>Native Name:</span> {country.name.official}</p>
+                <p><span>Population:</span> {country.name.official}</p>
+                <p>Region: {country.region}</p>
+                <p>Sub Region: {country.subregion}</p>
+                <p>Capital: {country.capital}</p>
+            </div>
+
+            <div className='country__secondary-info'>
+                <p>Top Level Donain: {country.name.official}</p>
+                <p>Currencies: {country.name.official}</p>
+                <p>Languages: {country.name.official}</p>
+            </div>
+
+
+            <button>Border Countries</button>
+
         </div>
-
-        <br></br>
-
-        <div className='country__secondary-info'>
-            <p>Top Level Donain: {country.name.official}</p>
-            <p>Currencies: {country.name.official}</p>
-            <p>Languages: {country.name.official}</p>
-        </div>
-
-
-        <button>Border Countries</button>
 
     </div>
   );
