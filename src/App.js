0be11692e91a301672b99c4656d0413f23680b7e
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-28 01:55:05 +0000

    update app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header.js';
+import Home from './pages/Home/Home.js';
 import Country from './pages/Country/Country';
 import { useSelector } from 'react-redux'
 
@@ -15,6 +16,16 @@ function App() {
         backgroundColor: '#f8f9fa',
     }
 }
+  // // api call
+  // const [countriesData, setCountriesData] = useState([])
+
+  // useEffect(() => {
+  //   const baseURL = `https://restcountries.com/v3.1/all`
+  //     axios.get(baseURL).then((response) => {
+  //       setCountriesData(response.data)
+  //     })
+  //     console.log(countriesData)
+  // }, [])
 
   return (
     <div  className="App"
