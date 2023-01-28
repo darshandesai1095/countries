8eca2a691631fbd1896f6057661df1af856c7383
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-01-28 01:54:57 +0000

    update country info

--- src/components/CountryInfo/CountryInfo.js
+++ src/components/CountryInfo/CountryInfo.js
@@ -10,24 +10,24 @@ console.log("COUNTRY: ", country)
   return (
     <div className='country-info'>
 
-        <img src={country.flags?.png}/>
+        <img src={country?.flags.png}/>
 
         <div className=''>
 
-            <h4>{country.name.common}</h4>
+            <h4>{country?.name.common}</h4>
 
             <div className='country__primary-info'>
-                <p><span>Native Name:</span> {country.name.official}</p>
-                <p><span>Population:</span> {country.name.official}</p>
-                <p>Region: {country.region}</p>
-                <p>Sub Region: {country.subregion}</p>
-                <p>Capital: {country.capital}</p>
+                <p><span>Native Name:</span> {country?.name.official}</p>
+                <p><span>Population:</span> {country?.name.official}</p>
+                <p>Region: {country?.region}</p>
+                <p>Sub Region: {country?.subregion}</p>
+                <p>Capital: {country?.capital}</p>
             </div>
 
             <div className='country__secondary-info'>
-                <p>Top Level Donain: {country.name.official}</p>
-                <p>Currencies: {country.name.official}</p>
-                <p>Languages: {country.name.official}</p>
+                <p>Top Level Donain: {country?.name.official}</p>
+                <p>Currencies: {country?.name.official}</p>
+                <p>Languages: {country?.name.official}</p>
             </div>
 
 
