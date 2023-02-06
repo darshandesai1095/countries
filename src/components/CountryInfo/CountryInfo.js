6ad3a4f8fc6ca8222f107762394b3f13e6042d6d
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-02-06 19:03:35 +0000

    add currencies

--- src/components/CountryInfo/CountryInfo.js
+++ src/components/CountryInfo/CountryInfo.js
@@ -4,14 +4,14 @@ import './CountryInfo.css'
 
 function CountryInfo() {
 
-const {countriesData, error, loading} = useApi()
+const {countriesData} = useApi()
 const country = countriesData[0]
 console.log(country)
 
-// const languagesObj = country?.languages
-// let languages = Object.keys(languagesObj).map(lang => languagesObj[lang]).join(', ')
+const languagesObj = country?.languages
+let languages = Object.keys(languagesObj).map(lang => languagesObj[lang]).join(', ')
 
-// const region = Object.keys(country?.currencies)[0]
+const region = Object.keys(country?.currencies)[0]
 
   return (
     <div className='country-info'>
@@ -32,8 +32,8 @@ console.log(country)
 
             <div className='country__secondary-info'>
                 <p>Top Level Domain: {country?.tld[0]}</p>
-                {/* <p>Currencies: {country?.currencies[region].name}</p> */}
-                {/* <p>Languages: {languages}</p> */}
+                <p>Currencies: {country?.currencies[region].name}</p>
+                <p>Languages: {languages}</p>
             </div>
 
             <button>Border Countries</button>
