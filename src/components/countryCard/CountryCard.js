e511a479eadeb76187641cab62d0cd0993112cac
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-03-09 16:29:09 -0600

    add country click link

--- src/components/countryCard/CountryCard.js
+++ src/components/countryCard/CountryCard.js
@@ -1,8 +1,12 @@
 import React from "react";
 import './CountryCard.css'
 import { useSelector, useDispatch } from 'react-redux'
+import { endpointSlice, setEndpoint } from "../../redux/features/endpointSlice";
+import { Link } from "react-router-dom";
 
 export default function CountryCard(props) {
+
+    const dispatch = useDispatch()
 
     const population = props.country.population.toLocaleString("en-US")
     const region = props.country.region
@@ -23,19 +27,21 @@ export default function CountryCard(props) {
     }
 
     return (
+
         <div    className="country-card" 
-                style={theme.darkTheme? countryCard.lightMode : countryCard.darkMode}
-                onClick={()=>console.log(`${props.country.name.common} clicked`)}
-        >
+                    style={theme.darkTheme? countryCard.lightMode : countryCard.darkMode}
+                    onClick={()=>dispatch(setEndpoint(`/name/${props.country.name.common}`))}
+            >
+        <Link to="country" style={{ textDecoration: 'none', color: "inherit" }}>
 
-            <img src={props.country.flags.png}/>
-            <div className="description">
-                <h4 className="title">{props.country.name.common}</h4>
-                <p><span className="bold">Population: </span>{population}</p>
-                <p><span className="bold">Region: </span>{region}</p>
-                <p><span className="bold">Capital: </span>{capital}</p>
-            </div>
-
+                <img src={props.country.flags.png}/>
+                <div className="description">
+                    <h4 className="title">{props.country.name.common}</h4>
+                    <p><span className="bold">Population: </span>{population}</p>
+                    <p><span className="bold">Region: </span>{region}</p>
+                    <p><span className="bold">Capital: </span>{capital}</p>
+                </div>
+        </Link>
         </div>
     )
 }
--- src/redux/features/endpointSlice.js
+++ src/redux/features/endpointSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = { 
     value: {
-        endpoint: 'name/belgium' //'all'
+        endpoint: 'all'
     },
 }
 
