c58d0afec0b73a7d21ff654487a66faf706472be
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-03-09 16:50:09 -0600

    back button functionality

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { setEndpoint } from '../../redux/features/endpointSlice'
+import { Link } from "react-router-dom";
 
 function BackButton() {
 
+  const dispatch = useDispatch()
+
   return (
-    <button>
-        Back
-    </button>
+    <div onClick={()=>dispatch(setEndpoint('all'))}>
+      <Link to="/" style={{ textDecoration: 'none', color: "inherit" }}>
+        <button>
+          Back
+        </button>
+      </Link>
+    </div>
+
   );
 }
 
