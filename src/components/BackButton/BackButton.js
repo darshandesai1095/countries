4a1da272499c77b384c4f28e9a8cec41329ce289
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-03-09 21:06:35 -0600

    back button functionality

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { setEndpoint } from '../../redux/features/endpointSlice'
 import { Link } from "react-router-dom";
+import {useNavigate} from "react-router-dom"
 
 function BackButton() {
 
+  const endpoint = useSelector(state => state.endpoint.value)
+
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const goBack = () => {
+    navigate(-1)
+    dispatch(setEndpoint('all'))
+  }
 
   return (
-    <div onClick={()=>dispatch(setEndpoint('all'))}>
-      <Link to="/" style={{ textDecoration: 'none', color: "inherit" }}>
+    <div onClick={()=>goBack()}>
+      {/* <Link to="/" style={{ textDecoration: 'none', color: "inherit" }}> */}
         <button>
           Back
         </button>
-      </Link>
+      {/* </Link> */}
     </div>
 
   );
