2a913e31793e3b8c2efecdaece1732289a229168
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-03-09 22:24:40 -0600

    back button styling

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -3,6 +3,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import { setEndpoint } from '../../redux/features/endpointSlice'
 import { Link } from "react-router-dom";
 import {useNavigate} from "react-router-dom"
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import "./BackButton.css"
+import { Scale } from '@mui/icons-material';
 
 function BackButton() {
 
@@ -16,11 +20,32 @@ function BackButton() {
     dispatch(setEndpoint('all'))
   }
 
+  const theme = useSelector((state) => state.theme.value)
+
+  const buttonStyle = {
+    light: {
+        backgroundColor: 'white',
+        boxShadow:'0px 0px 10px #dee2e6',
+        color: '#495057',
+        border: '1px solid white'
+
+    },
+
+    dark: {
+        backgroundColor: '#435567',
+        boxShadow:'0px 0px 10px #253747',
+        color: 'white',
+        border: '1px solid #435567'
+    }
+}
+
   return (
     <div onClick={()=>goBack()}>
       {/* <Link to="/" style={{ textDecoration: 'none', color: "inherit" }}> */}
-        <button>
-          Back
+        <button className="back-button" style={theme.darkTheme? buttonStyle.light : buttonStyle.dark}>
+          <ArrowBackIcon style={{transform:"Scale(0.7)"}}/>
+          {/* <span>&#8592;</span> */}
+          <span>Back</span>
         </button>
       {/* </Link> */}
     </div>
